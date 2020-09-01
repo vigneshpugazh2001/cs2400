6f786bad5ea0aa21e62226070c51745dccf4a040
Vignesh Pugazhenthi <vp995218@ohio.edu>
2020-09-01 18:01:36 -0400

    add git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Vignesh Pugazhenthi
+ *        Date: 9/1/2020
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
